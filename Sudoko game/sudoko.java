afa599d57803397580bf40af972dced6744bc1f2
Suresh-tutorial <sureshshanmugam1501@gmail.com>
2025-05-24 17:40:20 +0530

    code updated

--- Sudoko game/sudoko.java
+++ Sudoko game/sudoko.java
@@ -21,6 +21,14 @@ public class sudoko {
     }
     System.out.println("\nInput Sudoku:");
     printBorad(board);
+    if(solveSudoku(board)){
+        System.out.println("\nSolution:");
+        printBoard(board);
+    }
+    else{
+        System.out.println("\nNo Solution exists for this Sudoku Puzzle.");
+    }
+    scan.close();
     
    }
    public static void printboard(int [][] board){
@@ -39,6 +47,57 @@ public class sudoko {
         }
         System.out.println();
     }
+   }
+   public static boolean solveSudoku(int [][] board){
+    for(int row=0;row<SIZE;++row){
+        for(int col=0;col<SIZE; ++col){
+            if(board[row][col]==EMPTY){
+                for(int num=1;num<=SIZE;++num){
+                    if(isValid(board,row,col,num)){
+                        board[row][col]=num;
+                        //Recursive solve the rest of the board
+                        if(solveSudoku(board)){
+                            return true;
+                        }else{
+                            //if the current number doesn't lead a solution , backtrack
+                            board[row][col]=EMPTY;
+
+                        }
+                    }
+                }
+                //if no number works, backtrack
+                return false;
+            }
+        }
+    }
+    // if we've filled the entire board , the puzzle is solved
+    return true;
+   }
+   private static boolean isValid(int[][] board, int row, int col, int num){
+    //check row
+    for(int j=0; j<SIZE;++j){
+        if(board[row][j]==num){
+            return false;
+        }
+    }
+    //check column
+    for(int i=0;i<SIZE;++i){
+        if(borad[i][col]==num){
+            return false;
+        }
+    }
+    //check 3x3 box
+    int boxRow= row-row%3;
+    int boxCol=col-col%3;
+    for(int i=boxRow;i<boxRow+3;++i){
+        for(int j=boxCol;j<boxCol+3;++j){
+            if(board[i][j]==num){
+                return false;
+            }
+        }
+
+    }
+    return true;
    }
 
 
